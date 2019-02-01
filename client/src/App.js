a0eaf269e739f123b2c7ff6bbfcb8fa1ecdf391c
Jed Diaz <jemjemzms@gmail.com>
2019-02-02 12:38:42 +1300

    React & Apollo Setup
    Setting up React with concurrently, installing and setting up apollo and creating our launches query and component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import Launches from "./components/Launches";
+
+import "./App.css";
+import logo from "./logo.png";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+          <Launches />
+        </div>
+      </ApolloProvider>
     );
   }
 }
