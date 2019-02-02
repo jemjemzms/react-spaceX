bb9913ee515eb2141f736878e4b957ef8676cac3
Jed Diaz <jemjemzms@gmail.com>
2019-02-02 14:08:41 +1300

    Public Update
    Adding the public folder

---
+++ public/precache-manifest.5115d440d0b6f2063fc77d4e92ad0bb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "921014352cd4590a3648",
+    "url": "/static/js/main.92101435.chunk.js"
+  },
+  {
+    "revision": "98c67e2d020295d6dfc2",
+    "url": "/static/js/1.98c67e2d.chunk.js"
+  },
+  {
+    "revision": "921014352cd4590a3648",
+    "url": "/static/css/main.8aea6368.chunk.css"
+  },
+  {
+    "revision": "3e0ea9413af5824e62e17571116e811c",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const cors = require("cors");
 const schema = require("./schema");
-
+const path = require("path");
 const app = express();
 
 // Allow cross-origin
@@ -15,6 +15,12 @@ app.use(
     graphiql: true
   })
 );
+
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname__, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
