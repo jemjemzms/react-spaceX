dc3a22d1ae9ccd2d75755608d4e61486dc417b93
Jed Diaz <jemjemzms@gmail.com>
2019-02-02 13:38:16 +1300

    Launch Component
    Added color coded titles for success/fail missions, creted the Launch component and make another query to the GraphQL server from React/Apollo.

---
+++ client/src/components/MissionKey.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function MissionKey() {
+  return (
+    <div className="my-3">
+      <p>
+        <span className="px-3 m3-2 bg-success" />
+        &nbsp;=&nbsp;Success
+      </p>
+      <p>
+        <span className="px-3 m3-2 bg-danger" />
+        &nbsp;=&nbsp;Fail
+      </p>
+    </div>
+  );
+}
